41b0e60339db9f326931cb37a176ddc84c795c62
Harsh Telang <telangharsh06@gmail.com>
2024-03-01 21:02:53 +0530

    header basic designing complete

---
+++ frontend/src/app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='h-24 w-screen'>
+        <div className='h-full w-full bg-white'>
+            <div className='h-full w-56'>
+                <img src="/logoImage.jpg" alt="logo" className='h-full w-full' />
+            </div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
