c562a6527ff48c2616fdc4e58ffef15101cb281f
Harsh Telang <telangharsh06@gmail.com>
2024-03-04 21:15:28 +0530

    register route created

---
+++ frontend/src/app/api/auth/signup/route.ts
@@ -0,0 +1,37 @@
+import { db } from "@/lib/prismaDB";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+
+    const { fullName, email, password } = body;
+    if (!fullName || !email || !password) {
+      return NextResponse.json(
+        { message: "All fields are required" },
+        { status: 409 }
+      );
+    }
+
+    const existingUser = await db.user.findUnique({
+      where: { email: email },
+    });
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 409 }
+      );
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    const newUser = await db.user.create({
+      data: { fullName, email, password: hashedPassword },
+    });
+
+    return NextResponse.json({ user: newUser, message: "User created successfully"}, { status: 200 });
+  } catch (error) {
+    NextResponse.json(error);
+  }
+}
