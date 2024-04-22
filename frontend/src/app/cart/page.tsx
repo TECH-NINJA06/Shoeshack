a4adc2cc724d09d44ab7671cb43b974bff696dbb
Harsh Telang <telangharsh06@gmail.com>
2024-04-22 18:59:14 +0530

    cart Page Done

--- frontend/src/app/cart/page.tsx
+++ frontend/src/app/cart/page.tsx
@@ -4,11 +4,12 @@ import Link from "next/link";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RiDeleteBin5Line } from "react-icons/ri";
+import { removeCart } from "@/lib/redux/features/slices/cart/cartSlice";
 
 interface CartItem {
   id: string;
   title: string;
-  image: string;
+  itemImg: string;
   brand: string;
   price: number;
   size: number;
@@ -18,9 +19,10 @@ interface RootState {
   cartItems: CartItem[];
 }
 
-function Page () {
+function Page() {
   const dispatch = useDispatch();
   const cartItems = useSelector((state: RootState) => state.cartItems);
+  console.log(cartItems);
 
   const calculateTotalPrice = () => {
     if (cartItems.length === 0) {
@@ -33,6 +35,10 @@ function Page () {
     return totalPrice;
   };
 
+  const handleDeleteCart = (productId: string) => {
+    dispatch(removeCart(productId));
+  }
+
   return (
     <div className="size-full">
       <Navbar />
@@ -43,25 +49,39 @@ function Page () {
           </div>
           <div className="w-full h-20 flex justify-end gap-10 items-center border-b border-white pr-14">
             <p className="text-slate-300">Product</p>
-            <p className="text-slate-300">Price</p>
+            <p className="text-slate-300">Size</p>
             <p className="text-slate-300">Quantity</p>
             <p className="text-slate-300">SubTotal</p>
           </div>
           <div className="w-full h-72 overflow-y-scroll flex flex-col items-center border-b border-white pr-10 my-5 gap-2">
-            {/* {cartItems.map((cartItem) => {
+            {cartItems.map((cartItem) => {
+              const truncatedTitle =
+                cartItem.title.length > 8
+                  ? cartItem.title.substring(0, 8) + "..."
+                  : cartItem.title;
               return (
-                <div className="w-full h-[50%] bg-red-200">
-                  {cartItem.title}
+                <div className="w-full h-[45%] flex gap-4 items-center justify-between border-b">
+                  <div className="h-full w-[40%]  flex gap-16 items-center px-2">
+                    <button onClick={() => handleDeleteCart(cartItem.id)}>
+                      <RiDeleteBin5Line className="text-white" />
+                    </button>
+                    <div className="h-[95%] w-52 flex justify-center items-center">
+                      <img
+                        src={cartItem.itemImg}
+                        alt="item_Img"
+                        className="size-auto max-h-full"
+                      />
+                    </div>
+                  </div>
+                  <div className="h-full w-[53%] flex items-center gap-2">
+                    <p>{truncatedTitle}</p>
+                    <p className="pl-10">{cartItem.size}</p>
+                    <p className="px-16">{cartItem.quantity}</p>
+                    <p className="pl-4">{cartItem.price}</p>
+                  </div>
                 </div>
               );
-            })} */}
-            <div className="w-full h-[45%] bg-red-200 flex gap-4 items-center justify-between">
-                {/* <RiDeleteBin5Line className="text-white"/> */}
-                <div className="h-full w-[40%] bg-black"></div>
-                <div className="h-full w-[53%] bg-black flex items-center">
-
-                </div>
-            </div>
+            })}
           </div>
         </div>
         <div className="h-full w-[40%] flex flex-col justify-center items-center px-10">
@@ -92,6 +112,6 @@ function Page () {
       </div>
     </div>
   );
-};
+}
 
 export default Page;
