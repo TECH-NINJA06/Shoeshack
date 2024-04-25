c369abda3630122552ac1469fe9f71ac063704a3
Harsh Telang <125661668+TECH-NINJA06@users.noreply.github.com>
2024-04-25 22:09:21 +0530

    Update middleware.ts

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
  
-// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
  const path = request.nextUrl.pathname
 
