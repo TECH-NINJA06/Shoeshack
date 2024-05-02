f9cf0f9caa68fca09acdcb662822c6f8ab7ae739
Harsh Telang <telangharsh06@gmail.com>
2024-05-02 19:54:52 +0530

    cart controller updated

--- frontend/src/lib/redux/features/slices/cart/cartSlice.ts
+++ frontend/src/lib/redux/features/slices/cart/cartSlice.ts
@@ -33,30 +33,29 @@ export const cartSlice = createSlice({
                 quantity: 1
             }
             state.cartItems.push(item);
-            // axios.post("/api/cart", {productId: item.id, productSize: item.size});
+            axios.post("/api/cart", {productId: item.id, productSize: item.size});
         },
         removeCart: (state, action) => {
-          const item: CartItem = {
-            id: action.payload.id,
-            title: action.payload.title,
-            itemImg: action.payload.itemImg,
-            size: action.payload.size,
-            brand: action.payload.brand,
-            price: action.payload.price,
-            quantity: 1
-        }
+          const { id, size } = action.payload;
+        
+          // Find the index of the item to be removed
           const itemIndex = state.cartItems.findIndex(
-            (item) => item.id === action.payload.id && item.size === action.payload.size
+            (item) => item.id === id && item.size === size
           );
+        
+          // Remove the item from the cartItems array
           state.cartItems.splice(itemIndex, 1);
-          // axios.delete("/api/cart", {productId: item.id, productSize: item.size});
-        },
+        
+          // Make the API call to delete the item from the server-side cart
+          axios.delete("/api/cart", { data: { productId: id, productSize: size } })
+        },        
         updateCart: (state, action) => {
             const itemIndex = state.cartItems.findIndex(
                 (item) => item.id === action.payload.id && item.size === action.payload.size
               );
             if (action.payload.function === 'add') {
               state.cartItems[itemIndex].quantity += 1;
+              axios.post("/api/cart", {productId: action.payload.id, productSize: action.payload.size});
             } else if (action.payload.function === 'subtract') {
               state.cartItems[itemIndex].quantity -= 1
             }
