904a36f65083c876aec0102470d920e6802feb2a
Harsh Telang <telangharsh06@gmail.com>
2024-05-23 11:48:30 +0530

    style fixes

--- frontend/src/app/(home)/profile/[slug]/page.tsx
+++ frontend/src/app/(home)/profile/[slug]/page.tsx
@@ -56,67 +56,67 @@ const Page = () => {
   return (
     <div className="h-screen max-w-screen">
       <div className="flex justify-center items-center h-[100vh] w-screen">
-        <div className="h-[90vh] w-[30%] flex flex-col gap-5 py-5 border-2">
+        <div className="h-[90vh] sm:w-[30%] w-[70%] flex flex-col gap-5 py-5 border-2">
           <div className="w-full h-[40%] flex flex-col justify-evenly items-center">
-            <div className="size-28 rounded-full">
+            <div className="sm:size-28 size-20 rounded-full">
               <img
                 src="/navAvatar.jpg"
                 alt="hola"
                 className="size-full rounded-full"
               />
             </div>
-            <div className="w-full h-14 flex justify-center items-center text-white font-semibold ">
+            <div className="w-full h-14 flex justify-center items-center text-white font-semibold text-center">
               {profile?.fullName}
             </div>
           </div>
-          <div className="w-full h-[55%] flex flex-col gap-5 items-center pb-5">
-            <div className="w-full h-20 flex justify-center items-center gap-5">
-              <div className="h-14 w-32 bg-white rounded">
+          <div className="w-full h-[55%] flex flex-col sm:gap-5 gap-2 items-center pb-5 sm:px-0 px-2">
+            <div className="w-full sm:h-20 h-16 flex sm:justify-center justify-between items-center sm:gap-5">
+              <div className="sm:h-14 h-10 sm:w-32 w-24 bg-white rounded">
                 <button
                   onClick={() => {
                     router.push(`/avatarChange`);
                   }}
-                  className="size-full font-semibold"
+                  className="size-full font-bold text-xs sm:text-lg"
                 >
                   Change Avatar
                 </button>
               </div>
-              <div className="h-14 w-32 bg-white rounded">
+              <div className="sm:h-14 h-10 sm:w-32 w-24 bg-white rounded">
                 <button
                   onClick={handleLogout}
-                  className="size-full font-semibold"
+                  className="size-full font-bold text-xs sm:text-lg"
                 >
                   Logout
                 </button>
               </div>
             </div>
-            <div className="w-full h-20 flex justify-center items-center gap-5">
-              <div className="h-14 w-32 bg-white rounded">
+            <div className="w-full sm:h-20 h-16 flex justify-center items-center gap-5">
+              <div className="sm:h-14 h-10 sm:w-32 w-24 bg-white rounded">
                 <button
                   onClick={() => {
                     router.push(`/cart`);
                   }}
-                  className="size-full font-semibold"
+                  className="size-full font-bold text-xs sm:text-lg"
                 >
                   Cart
                 </button>
               </div>
-              <div className="h-14 w-32 bg-white rounded">
+              <div className="sm:h-14 h-10 sm:w-32 w-24 bg-white rounded">
                 <button
                   onClick={()=>{router.push('/orders')}}
-                  className="size-full font-semibold"
+                  className="size-full font-bold text-xs sm:text-lg"
                 >
                   My Orders
                 </button>
               </div>
             </div>
-            <div className="w-full h-20 flex justify-center items-center gap-5">
-              <div className="h-14 w-32 bg-white rounded">
+            <div className="w-full sm:h-20 h-16 flex justify-center items-center gap-5">
+              <div className="sm:h-14 h-10 sm:w-32 w-24 bg-white rounded">
                 <button
                   onClick={() => {
                     router.push(`/home`);
                   }}
-                  className="size-full font-semibold"
+                  className="size-full font-bold text-xs sm:text-lg"
                 >
                   Home
                 </button>
