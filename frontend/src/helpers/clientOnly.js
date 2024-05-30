d1b8494521de1f0908cda3628f8b80512a456b78
Harsh Telang <telangharsh06@gmail.com>
2024-05-30 08:53:23 +0530

    fix - bug fixes

---
+++ frontend/src/helpers/clientOnly.js
@@ -0,0 +1,17 @@
+'use client';
+
+import { ReactNode, useEffect, useState } from 'react';
+
+export default function ClientOnly({ children }) {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  return <>{children}</>;
+}
